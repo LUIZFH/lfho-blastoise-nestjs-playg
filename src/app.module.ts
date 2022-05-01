f43fe2c50968e9669fef1c18360cc63fa451b452
Luiz Filho <luiz.pereira@nuvemshop.com.br>
2022-05-01 19:58:08 -0300

    criacao do modulo pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonController } from './pokemon/pokemon.controller';
+import { PokemonService } from './pokemon/pokemon.service';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [HttpModule, PokemonModule,],
   controllers: [AppController, PokemonController],
-  providers: [AppService],
+  providers: [AppService, PokemonService],
 })
 export class AppModule {}
