df1566045505aee05f48a894f0ab465f7236e0df
luizfilho46 <luizcarlos.pereirafilho@yahoo.com.br>
2022-05-01 14:37:34 -0300

    commit inicial da api para experimento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonController } from './pokemon/pokemon.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PokemonController],
   providers: [AppService],
 })
 export class AppModule {}
